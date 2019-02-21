82e97c19412c95260fc9ec2cf0817a31627efacf
sakthigowtham <42170124+sakthigowtham@users.noreply.github.com>
2019-02-21 10:49:49 +0530

    Create jai.java

---
+++ jai.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class vow 
+{
+    public static void main(String[] args) 
+{
+Scanner s=new Scanner(System.in);
+char ch =s.next().charAt(0);
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println( " vowel");
+        else
+            System.out.println( " consonant");
+
+   }
+}
